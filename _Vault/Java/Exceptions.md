3eb6e71095f14fc7a1d98e8297c290d033c22c49
ram0973 <ram0973@gmail.com>
2022-07-20 13:29:09 +0300

    another commit

--- _Vault/Java/Exceptions.md
+++ _Vault/Java/Exceptions.md
@@ -3,3 +3,12 @@ graph TB
 Throwable --> Exception
 Throwable --> Error
 ```
+Exceptions are divided into checked and unchecked. If the method can throw a controlled type exception and does not handle it, then in the signature of the method you must specify the class the thrown exception: 
+
+`void someMethod throws Exception`
+
+```mermaid
+graph TB
+Exceptions --> checked
+Exceptions --> unchecked
+```
--- sources/java/src/main/java/exceptions/ExceptionExample.java
+++ sources/java/src/main/java/exceptions/ExceptionExample.java
@@ -3,11 +3,23 @@ package exceptions;
 public class ExceptionExample {
     public static void main(String[] args) {
         try {
-            int x = 3/0;
-        } catch (Throwable ex) {
-            for (StackTraceElement x : ex.getStackTrace()) {
-                System.out.println(x.toString());
+            try {
+                System.out.println("1st try");
+                int x = 3 / 0;
+            } catch (ArithmeticException ex) {
+                System.out.println("1st catch");
+                throw new ArrayIndexOutOfBoundsException();
+            } catch (ArrayIndexOutOfBoundsException ex) {
+                System.out.println("2nd catch");
+            } finally {
+                System.out.println("1st finally");
+                String[] strings = {"123"};
+                strings[1] = "456";
             }
+        } catch (ArrayIndexOutOfBoundsException ex) {
+            System.out.println("3rd catch");
+        } finally {
+            System.out.println("2nd finally");
         }
     }
 
